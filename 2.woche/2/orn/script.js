09552e5301421100d32c0f763c0c3925fc23b7ab
Orhan Guezel <orhan.guezel@dci-student.org>
2024-11-26 22:34:56 +0100

    über event 10 aufgabe gemacht

--- 2.woche/2/orn/script.js
+++ 2.woche/2/orn/script.js
@@ -29,3 +29,81 @@ allLi.forEach(li => {
         console.log(li.innerText);
     });
 });
+
+// soru -5
+
+const addItem = document.getElementById('add-item');
+addItem.addEventListener('click', function () {
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+
+    // Listenin mevcut eleman sayısını kontrol et
+    const currentItems = ul.children.length;
+    const nextItemNumber = currentItems + 1;
+
+    li.innerText = `Item ${nextItemNumber}`; // Sıraya uygun numara eklenir
+    li.style.color= 'red';
+    ul.appendChild(li);
+});
+
+const removeItem = document.getElementById('remove-item');
+removeItem.addEventListener('click', function () {
+    const ul = document.querySelector('ul');
+    const lastLi = ul.lastElementChild;
+
+    if (lastLi) {
+        ul.removeChild(lastLi);
+    } else {
+        alert('List is already empty!');
+    }
+});
+
+// soru -6
+const externalLink = document.getElementById('external-link');
+externalLink.addEventListener('click',function(event){
+    event.preventDefault();
+    console.log('Link tıklanması engellendi!');
+});
+
+// soru -7
+
+const button = document.querySelectorAll('button');
+button.forEach(btn=>{
+    const firstBtn= btn.textContent;
+    btn.addEventListener('mouseover',function(){
+        btn.textContent = 'Mouse Over';
+    });
+    btn.addEventListener('mouseout',function(){
+        btn.textContent = firstBtn;
+    });
+}
+);
+
+// soru -8
+const dynamicList = document.getElementById('dynamic-list');
+const dynamicInput = dynamicList.querySelectorAll('li')
+dynamicInput.forEach(li=>{
+    li.addEventListener('click',function(){
+        li.innerText = 'Clicked!';
+        console.log(li.innerText);
+    });
+});
+
+
+// soru -9
+const nameInput2 = document.getElementById('name-input');
+const nameDisplay2 = document.getElementById('name-display');
+nameInput2.addEventListener('focus',function(){
+    nameDisplay2.style.color='blue';
+});
+
+// soru -10
+
+nameInput2.addEventListener('blur', function () {
+    nameDisplay2.style.color = 'black'; 
+});
+
+
+
+
+
